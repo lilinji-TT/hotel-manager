a7e5bf39dbd6ce93b69f3578d9bc42fa546c0aad
lilinji-TT <112566451+lilinji-TT@users.noreply.github.com>
2023-04-21 14:04:49 +0800

    eslint plugin 配置

--- hotel-app/.eslintrc.cjs
+++ hotel-app/.eslintrc.cjs
@@ -1,15 +1,23 @@
 module.exports = {
-  env: { browser: true, es2020: true },
-  extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:react-hooks/recommended',
-  ],
-  parser: '@typescript-eslint/parser',
-  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
-  plugins: ['react-refresh'],
-  rules: {
-    'react-refresh/only-export-components': 'warn',
-    'no-console':'error',
-  },
+	env: { browser: true, es2020: true },
+	extends: [
+		'eslint:recommended',
+		'plugin:@typescript-eslint/recommended',
+		'plugin:react-hooks/recommended',
+		'prettier',
+		'plugin:prettier/recommended',
+	],
+	parser: '@typescript-eslint/parser',
+	parserOptions: {
+		ecmaFeatures: {
+			jsx: true,
+		},
+		ecmaVersion: 'latest',
+		sourceType: 'module',
+	},
+	plugins: ['react-refresh', '@typescript-eslint', 'prettier'],
+	rules: {
+		'react-refresh/only-export-components': 'warn',
+		'no-console': 'error',
+	},
 }
--- hotel-app/src/App.tsx
+++ hotel-app/src/App.tsx
@@ -2,7 +2,15 @@ import { useState } from 'react'
 import './App.css'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-
+//测试是否会报错
+console.log(viteLogo)
+var a = 1
+var b = 1
+var c = 1
+var d = 1
+var e = 1
+var f = 1
+var g = 1
 function App() {
 	const [count, setCount] = useState(0)
 	return (
