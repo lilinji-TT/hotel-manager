6211c714dc0f643a9520d062e84e99a91add14ee
崔腾宇 <93563190+Freedom-Loneliness@users.noreply.github.com>
2023-04-22 22:49:24 +0800

    fix:修复了路由无法加载provider问题

--- hotel-app/src/App.tsx
+++ hotel-app/src/App.tsx
@@ -1,5 +1,5 @@
 import React, { useContext } from 'react'
-import { Navigate, Route, Routes } from 'react-router-dom'
+import { BrowserRouter as Router, Navigate, Route, Routes } from 'react-router-dom'
 import { UserContext } from './provider/UserProvider'
 import 'bootstrap/dist/css/bootstrap.css'
 import DashBoardPage from './components/dashBoard/dashboard'
@@ -8,21 +8,21 @@ import LoginPage from './components/authentication/login'
 const App: React.FC = (): ReactElement => {
 	const { userState } = useContext(UserContext)
 	const unauthorizedRoutes = (
-		<Route>
-			<Route path='/login' element={LoginPage} />
-			<Route path='/register' element={LoginPage} />
-			<Route path='/reset-password' element={LoginPage} />
+		<Routes>
+			<Route path='/login' element={<LoginPage />} />
+			<Route path='/register' element={<LoginPage />} />
+			<Route path='/reset-password' element={<LoginPage />} />
 			<Route path='*' element={<Navigate to='/login' replace />} />
-		</Route>
+		</Routes>
 	)
 	const authorizedRoutes = (
-		<Route>
-			<Route path='/' element={DashBoardPage} />
+		<Routes>
+			<Route path='/' element={<DashBoardPage />} />
 			<Route path='/login' element={<Navigate to='/' replace />} />
-		</Route>
+		</Routes>
 	)
 	if (!userState) return <>出错了</>
-	return <Routes>{userState.isAuthenticated ? authorizedRoutes : unauthorizedRoutes}</Routes>
+	return <Router>{userState.isAuthenticated ? authorizedRoutes : unauthorizedRoutes}</Router>
 }
 
 export default App
