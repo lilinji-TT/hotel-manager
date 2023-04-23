a48a220e0c0845d2100070e5e4689ba82d694507
lilinji-TT <112566451+lilinji-TT@users.noreply.github.com>
2023-04-23 16:20:51 +0800

    封装axios

---
+++ hotel-app/src/utils/request/axios.ts
@@ -0,0 +1,28 @@
+import axios, { type AxiosResponse } from 'axios'
+
+const service = axios.create({
+	baseURL: 'http://localhost:3002',
+	timeout: !isNaN(5000) ? Number(5000) : 60 * 1000
+})
+
+service.interceptors.request.use(
+	(config) => {
+		return config
+	},
+	(error) => {
+		return Promise.reject(error.response)
+	}
+)
+
+service.interceptors.response.use(
+	(response: AxiosResponse): AxiosResponse => {
+		if (response.status === 200) return response
+
+		throw new Error(response.status.toString())
+	},
+	(error) => {
+		return Promise.reject(error)
+	}
+)
+
+export default service
---
+++ hotel-app/src/utils/request/index.ts
@@ -0,0 +1,54 @@
+import type { AxiosResponse } from 'axios'
+import request from './axios'
+
+export interface HttpOption {
+	url: string
+	data?: unknown
+	method?: string
+	headers?: unknown
+}
+
+export interface Response<T = unknown> {
+	data: T
+	message: string | null
+	status: string
+}
+
+function http<T = unknown>({ url, data, method }: HttpOption) {
+	const successHandler = (res: AxiosResponse<Response<T>>) => {
+		if (res.data.status === 'Success') return res.data
+
+		return Promise.reject(res.data)
+	}
+
+	const failHandler = (error: Response<Error>) => {
+		throw new Error(error?.data.message || '对不起，出现了一点小错误~')
+	}
+
+	method = method || 'GET'
+
+	const params = Object.assign(typeof data === 'function' ? data() : data ?? {}, {})
+
+	return method === 'GET'
+		? request.get(url, { params }).then(successHandler, failHandler)
+		: request.post(url, params).then(successHandler, failHandler)
+}
+
+export function get<T = unknown>({ url, data, method = 'GET' }: HttpOption): Promise<Response<T>> {
+	return http<T>({
+		url,
+		method,
+		data
+	})
+}
+
+export function post<T = unknown>({ url, data, method = 'POST', headers }: HttpOption): Promise<Response<T>> {
+	return http<T>({
+		url,
+		method,
+		data,
+		headers
+	})
+}
+
+export default post
