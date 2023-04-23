5b5455baafbb192e4216bd2a0b7ee0b695efe194
崔腾宇 <93563190+Freedom-Loneliness@users.noreply.github.com>
2023-04-23 13:05:41 +0800

    feat:配置了record和room的provider

---
+++ hotel-app/src/provider/RoomProvider.tsx
@@ -0,0 +1,29 @@
+import { ReactNode, createContext, useReducer } from 'react'
+import { Room, RoomType, RoomStatus } from '../domin/Room.ts'
+const defaultRoomState: Room[] = [
+	{
+		_id: '',
+		type: RoomType.NONE,
+		number: '',
+		price: 0,
+		status: RoomStatus.OCCUPIED
+	}
+]
+
+const roomReducer = (state = defaultRoomState, action = {}) => {
+	switch (action.type) {
+		case '':
+			return
+		default:
+			return state
+	}
+}
+
+export const RoomContext = createContext({})
+
+const RoomProvider = ({ children }: { children: ReactNode | ReactNode[] }) => {
+	const [roomState, roomDispatch] = useReducer(roomReducer, defaultRoomState)
+	return <RoomContext.Provider value={{ roomState, roomDispatch }}>{children}</RoomContext.Provider>
+}
+
+export default RoomProvider
