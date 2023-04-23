28520a01e3a6c2957fda1de283bdd6517c5aec58
lilinji-TT <112566451+lilinji-TT@users.noreply.github.com>
2023-04-23 14:54:12 +0800

    登录页

--- hotel-app/src/components/authentication/login.tsx
+++ hotel-app/src/components/authentication/login.tsx
@@ -1,6 +1,6 @@
+import { LockOutlined, UserOutlined } from '@ant-design/icons'
+import { Button, Checkbox, Form, Input } from 'antd'
 import React, { useState } from 'react'
-import { Form, Input, Button } from 'antd'
-import { UserOutlined, LockOutlined } from '@ant-design/icons'
 
 const Login: React.FC = () => {
 	const [loading, setLoading] = useState<boolean>(false)
@@ -14,19 +14,47 @@ const Login: React.FC = () => {
 	}
 
 	return (
-		<Form name='login-form' onFinish={onFinish} className='login-form'>
-			<Form.Item name='username' rules={[{ required: true, message: 'Please input your username!' }]}>
-				<Input prefix={<UserOutlined />} placeholder='Username' />
-			</Form.Item>
-			<Form.Item name='password' rules={[{ required: true, message: 'Please input your password!' }]}>
-				<Input.Password prefix={<LockOutlined />} placeholder='Password' />
-			</Form.Item>
-			<Form.Item>
-				<Button type='primary' htmlType='submit' className='login-form-button' loading={loading}>
-					Log in
-				</Button>
-			</Form.Item>
-		</Form>
+		<div className='d-flex'>
+			<img
+				className='object-fit-cover w-100 h-100 position-absolute'
+				alt='酒店'
+				src='https://sitecore-cd-imgr.shangri-la.com/MediaFiles/6/F/E/{6FED1BBC-1D58-4F53-AB2C-59CFC918AB91}1920-800.jpg'
+			/>
+			<div
+				className='border border-2 rounded-4 d-flex justify-content-center align-items-center bg-white'
+				style={{ height: '600px', width: '400px', margin: '200px 7% 0 auto', zIndex: 1 }}
+			>
+				<Form name='normal_login' initialValues={{ remember: true }} onFinish={onFinish} style={{ width: '70%' }}>
+					<Form.Item name='title'>
+						<h1>登录</h1>
+					</Form.Item>
+					<Form.Item name='username' rules={[{ required: true, message: 'Please input your Username!' }]}>
+						<Input prefix={<UserOutlined className='site-form-item-icon' />} placeholder='Username' />
+					</Form.Item>
+					<Form.Item name='password' rules={[{ required: true, message: 'Please input your Password!' }]}>
+						<Input prefix={<LockOutlined className='site-form-item-icon' />} type='password' placeholder='Password' />
+					</Form.Item>
+					<Form.Item>
+						<Form.Item name='remember' valuePropName='checked' noStyle>
+							<Checkbox>记住我</Checkbox>
+						</Form.Item>
+
+						<a className='login-form-forgot' href=''>
+							忘记密码
+						</a>
+					</Form.Item>
+
+					<Form.Item>
+						<Button type='primary' htmlType='submit' className='login-form-button' loading={loading}>
+							登录
+						</Button>
+						<br />
+						<br />
+						Or <a href='https://www.bilibili.com/'>现在注册!</a>
+					</Form.Item>
+				</Form>
+			</div>
+		</div>
 	)
 }
 
