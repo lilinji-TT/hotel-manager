035de39068cf379cab0f607c22e2d261ffb7954e
lilinji-TT <112566451+lilinji-TT@users.noreply.github.com>
2023-04-25 16:43:13 +0800

    page route config with  register and forget

--- hotel-app/src/components/authentication/login.tsx
+++ hotel-app/src/components/authentication/login.tsx
@@ -1,24 +1,25 @@
-import * as React from 'react'
+import LockOutlinedIcon from '@mui/icons-material/LockOutlined'
 import Avatar from '@mui/material/Avatar'
+import Box from '@mui/material/Box'
 import Button from '@mui/material/Button'
-import CssBaseline from '@mui/material/CssBaseline'
-import TextField from '@mui/material/TextField'
-import FormControlLabel from '@mui/material/FormControlLabel'
 import Checkbox from '@mui/material/Checkbox'
+import CssBaseline from '@mui/material/CssBaseline'
+import FormControlLabel from '@mui/material/FormControlLabel'
+import Grid from '@mui/material/Grid'
 import Link from '@mui/material/Link'
 import Paper from '@mui/material/Paper'
-import Box from '@mui/material/Box'
-import Grid from '@mui/material/Grid'
-import LockOutlinedIcon from '@mui/icons-material/LockOutlined'
-import Typography from '@mui/material/Typography'
 import { createTheme, ThemeProvider } from '@mui/material/styles'
+import TextField from '@mui/material/TextField'
+import Typography from '@mui/material/Typography'
+import * as React from 'react'
 
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
 function Copyright(props: any) {
 	return (
 		<Typography variant='body2' color='text.secondary' align='center' {...props}>
 			{'Copyright © '}
-			<Link color='inherit' href='https://mui.com/'>
-				Your Website
+			<Link color='inherit' href='#'>
+				CLRY
 			</Link>{' '}
 			{new Date().getFullYear()}
 			{'.'}
@@ -32,6 +33,7 @@ export default function SignInSide() {
 	const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
 		event.preventDefault()
 		const data = new FormData(event.currentTarget)
+		// eslint-disable-next-line no-console
 		console.log({
 			email: data.get('email'),
 			password: data.get('password')
@@ -48,7 +50,7 @@ export default function SignInSide() {
 					sm={4}
 					md={7}
 					sx={{
-						backgroundImage: 'url(https://source.unsplash.com/random)',
+						backgroundImage: 'url(https://n.sinaimg.cn/sinacn21/214/w2048h1366/20180719/dd8d-hfnsvzc0685403.jpg)',
 						backgroundRepeat: 'no-repeat',
 						backgroundColor: (t) => (t.palette.mode === 'light' ? t.palette.grey[50] : t.palette.grey[900]),
 						backgroundSize: 'cover',
@@ -98,14 +100,12 @@ export default function SignInSide() {
 							</Button>
 							<Grid container>
 								<Grid item xs>
-									<Link href='#' variant='body2'>
+									<Link href='/reset-password' variant='body2'>
 										Forgot password?
 									</Link>
 								</Grid>
 								<Grid item>
-									<Link href='#' variant='body2'>
-										{"Don't have an account? Sign Up"}
-									</Link>
+									<Link href={'/register'}>{'Sign Up'}</Link>
 								</Grid>
 							</Grid>
 							<Copyright sx={{ mt: 5 }} />
