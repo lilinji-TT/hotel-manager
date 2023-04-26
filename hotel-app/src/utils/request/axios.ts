6d3e7b5ff6855cefc2b01af8771c1e70bb9c1113
崔腾宇 <93563190+Freedom-Loneliness@users.noreply.github.com>
2023-04-26 14:59:15 +0800

    feat:封装了简单axios

--- hotel-app/src/utils/request/axios.ts
+++ hotel-app/src/utils/request/axios.ts
@@ -1,28 +1,33 @@
-import axios, { type AxiosResponse } from 'axios'
+import axios from 'axios'
 
-const service = axios.create({
-	baseURL: 'http://localhost:3010',
-	timeout: !isNaN(5000) ? Number(5000) : 60 * 1000
+const request = axios.create({
+	baseURL: 'https://example.com/api',
+	timeout: 5000,
+	headers: { 'Content-Type': 'application/json' }
 })
 
-service.interceptors.request.use(
+// 添加请求拦截器
+request.interceptors.request.use(
 	(config) => {
+		// 在发送请求之前做些什么
+		// 添加请求头
 		return config
 	},
 	(error) => {
-		return Promise.reject(error.response)
+		// 对请求错误做些什么
+		return Promise.reject(error)
 	}
 )
 
-service.interceptors.response.use(
-	(response: AxiosResponse): AxiosResponse => {
-		if (response.status === 200) return response
-
-		throw new Error(response.status.toString())
+// 添加响应拦截器
+request.interceptors.response.use(
+	(response) => {
+		// 对响应数据做点什么
+		return response
 	},
 	(error) => {
 		return Promise.reject(error)
 	}
 )
 
-export default service
+export default request
--- hotel-app/src/utils/request/index.ts
+++ hotel-app/src/utils/request/index.ts
@@ -1,58 +1,20 @@
-import type { AxiosResponse } from 'axios'
 import request from './axios'
-export interface HttpOption {
-	url: string
-	data?: unknown
-	method?: string
-	headers?: unknown
-}
 
-export interface Response<T = unknown> {
-	data: T
-	message: string | null
-	status: string
-	response: {
-		data: {
-			status: string
-			message: string | null
-		}
-	}
-}
-
-function http<T = unknown>({ url, data, method }: HttpOption) {
-	const successHandler = (res: AxiosResponse<Response<T>>) => {
-		if (res.data?.status === 'Success') return res.data
-		return Promise.reject(res.data)
-	}
-
-	const failHandler = (error: Response<Error>) => {
-		throw new Error(error.response?.data.message || '对不起，出现了一点小错误~')
-	}
-
-	method = method || 'GET'
-
-	const params = Object.assign(typeof data === 'function' ? data() : data ?? {}, {})
-
-	return method === 'GET'
-		? request.get(url, { params }).then(successHandler, failHandler)
-		: request.post(url, params).then(successHandler, failHandler)
-}
-
-export function get<T = unknown>({ url, data, method = 'GET' }: HttpOption): Promise<Response<T>> {
-	return http<T>({
-		url,
-		method,
-		data
+// 封装get请求
+export const get = (url, params = {}) => {
+	return request.get(url, {
+		params
 	})
 }
 
-export function post<T = unknown>({ url, data, method = 'POST', headers }: HttpOption): Promise<Response<T>> {
-	return http<T>({
-		url,
-		method,
-		data,
-		headers
-	})
+// 封装post请求
+export const post = (url, data = {}) => {
+	return request.post(url, data)
 }
 
-export default post
+// 封装delete请求
+export const del = (url, params = {}) => {
+	return request.delete(url, {
+		params
+	})
+}
